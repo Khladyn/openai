924c4b6e83a5b473a70b1ec5cc58f3e7a0aac2c8
James Sing <87142332+ambivjaps@users.noreply.github.com>
2024-04-20 20:03:37 +0800

    Updated cards

--- ICS26015/finals_activity_1_input.php
+++ ICS26015/finals_activity_1_input.php
@@ -3,15 +3,20 @@
         <div class="container px-lg-5 p-3 mt-5">
             
             <div class="text-center text-light my-4">
+                <h2 class="fw-bold">Welcome.</h2>
                 <h3>Pick a form to fill and start generating something!</h3>
             </div>
 
             <div class="row">
-                <div class="col-lg-4 col-md-12 col-12 my-4">
+                <div class="col-xl-4 col-lg-12 col-md-12 col-12 my-4">
                     <!-- Programming Language -->
-                    <div class="cat1 bg-primary p-5 rounded shadow-lg">
-                        <h1 class="text-center"><i class="bi bi-code-slash"></i></h1>
-                            <h2 class="text-center fw-bold mb-5">Programming Language</h2>
+                    <div class="cat1 rounded shadow-lg">
+                        <div class="head bg-primary rounded-top">
+                            <h5 class="fw-bold mx-2"><i class="bi bi-code-slash fa-lg"></i> &nbsp; Programming Language</h5>
+                        </div>
+                        <div class="content p-4">
+                            <p> Lorem ipsum dolor sit amet.. </p>
+                            <!-- Form -->
                             <form id="programming-language" action="finals_activity_1_controller.php" method="post">
                                 <div class="form-group">
                                     <label for="language">Select a programming language:</label>
@@ -33,41 +38,52 @@
                                         <option value="Haskell">Haskell</option>
                                     </select>
                                 </div>
-                                <div class="form-group my-3">
+                                <div class="form-group my-2">
                                     <label for="task">Enter an equation:</label>
                                     <input type="text" class="form-control" id="task" name="task" required>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-warning fw-bold w-100">Generate Result</button>
+                                <button type="submit" class="btn btn-primary btn-warning fw-bold w-100 mt-3"><i class="bi bi-caret-right-fill"></i> Generate Result</button>
                             </form>
                         </div>
-                </div>
-
-                <div class="col-lg-4 col-md-12 col-12 my-4">
-                    <!-- Recipe Generator -->
-                    <div class="cat2 bg-success p-5 rounded shadow-lg">
-                        <h1 class="text-center"><i class="bi bi-egg-fried fa-lg"></i></h1>
-                        <h2 class="text-center fw-bold mb-5">Recipe <br> Generator</h2>
-                        <form id="recipe-generator" action="finals_activity_1_controller.php" method="post">
-                            <div class="form-group my-3">
-                                <label for="dish">Enter a dish:</label>
-                                <input type="text" class="form-control" id="dish" name="dish" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-warning fw-bold w-100">Generate Recipe</button>
-                        </form>
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-12 col-12 my-4">
-                    <div class="cat3 bg-danger p-5 rounded shadow-lg">
-                        <h1 class="text-center"><i class="bi bi-collection-play-fill"></i></h1>
-                        <h2 class="text-center fw-bold mb-5">Social <br> Media Ads</h2>
-                        <form id="social-media-ads" action="finals_activity_1_controller.php" method="post">
-                            <div class="form-group my-3">
-                                <label for="ad">Enter a prompt:</label>
-                                <input type="text" class="form-control" id="ad" name="ad" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-warning fw-bold w-100">Generate Ad</button>
-                        </form>
+                <div class="col-xl-4 col-lg-12 col-md-12 col-12 my-4">
+                    <!-- Recipe Generator -->
+                    <div class="cat2 rounded shadow-lg">
+                        <div class="head bg-success rounded-top">
+                            <h5 class="fw-bold mx-2"><i class="bi bi-egg-fried fa-lg"></i> &nbsp; Recipe Generator</h5>
+                        </div>
+                        <div class="content p-4">
+                            <p> Lorem ipsum dolor sit amet.. </p>
+                            <!-- Form -->
+                            <form id="recipe-generator" action="finals_activity_1_controller.php" method="post">
+                                <div class="form-group my-2">
+                                    <label for="dish">Enter a dish:</label>
+                                    <input type="text" class="form-control" id="dish" name="dish" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-warning fw-bold w-100 mt-3"><i class="bi bi-caret-right-fill"></i> Generate Recipe</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-12 col-md-12 col-12 my-4">
+                    <div class="cat3 rounded shadow-lg">
+                        <div class="head bg-danger rounded-top">
+                            <h5 class="fw-bold mx-2"><i class="bi bi-collection-play-fill fa-lg"></i> &nbsp; Social Media Ads</h5>
+                        </div>
+                        <div class="content p-4">
+                            <p> Lorem ipsum dolor sit amet.. </p>
+                            <!-- Form -->
+                            <form id="social-media-ads" action="finals_activity_1_controller.php" method="post">
+                                <div class="form-group my-2">
+                                    <label for="ad">Enter a prompt:</label>
+                                    <input type="text" class="form-control" id="ad" name="ad" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-warning fw-bold w-100 mt-3"><i class="bi bi-caret-right-fill"></i> Generate Ad</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
