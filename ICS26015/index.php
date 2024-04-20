78a027ccce1c69750f4f34b3bdfd912f68ba7f31
James Sing <87142332+ambivjaps@users.noreply.github.com>
2024-04-20 19:38:19 +0800

    Added layouts folder

---
+++ ICS26015/index.php
@@ -0,0 +1,8 @@
+<?php 
+    /* Finals Activity#1 for ICS26015
+    Made by Jean Khladyn Stefanie Abarquez */
+
+    require 'layouts/header.php';
+    require 'finals_activity_1_input.php';
+    require 'layouts/footer.php';
+?>
---
+++ ICS26015/layouts/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Finals Activity#1 | Form Instances</title>
+
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="assets/img/apple-touch-icon.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/img/favicon-192x192.png" type="image/x-icon" sizes="192x192">
+        <link rel="apple-touch-icon-precomposed" href="assets/img/favicon-512x512.png" type="image/x-icon" sizes="512x512">
+        <link rel="manifest" href="assets/img/site.webmanifest">
+
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css?<?php echo time(); ?>">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    </head>
+
+    <body class="d-flex flex-column min-vh-100">
+        <!-- Navigation Header -->
+        <nav class="navbar bg-light navbar-expand-lg bg-body-tertiary sticky-top shadow-lg" data-bs-theme="light">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-bold" href="index.php">Finals Activity#1</a>
+                <p class="mx-3 my-2 d-none d-md-block fw-semibold"> Jean Khladyn Stefanie L. Abarquez / 4-ITC </p>
+            </div>
+        </nav>
